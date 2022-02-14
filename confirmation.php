f3d0721df33f723affb4a37cf6828a75714d1119
lowebang <96456184+lowebang@users.noreply.github.com>
2022-02-13 20:59:28 -0600

    Update and rename confirmation.html to confirmation.php

--- confirmation.php
+++ confirmation.php
@@ -10,6 +10,17 @@
 </head>
 
 <body>
+	<?php
+	$email = $_GET("email");
+	$to      = email;
+	$subject = 'Teen Entrepreneurship Webinar - Confirmation';
+	$message = 'You\'re all set! \n \n You are signed up for the Teen Entrepreneurship Webinar. We will send you a proper ZOOM code when the deadline comes up.';
+	$headers = 'From: BlissByBailee'       . "\r\n" .
+		 'Reply-To: ' + email + . "\r\n" .
+		 'X-Mailer: PHP/' . phpversion();
+
+	mail($to, $subject, $message, $headers);
+	?>
 	<div class="banner">
 		<div class="navbar">
 			<a href="index.html"><img src="images/favicon.ico" class="logo" alt="tsa.html"></a>
